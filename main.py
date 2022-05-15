a79267e0096dbbf5f370ac9f5257e22ae07f411b
Smorb <egor.yankof@gmail.com>
2022-05-15 17:12:15 +0300

    main

--- main.py
+++ main.py
@@ -35,6 +35,19 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
+#создания мяча и ракетки   
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
 #флаги, отвечающие за состояние игры
 game = True
 finish = False
@@ -48,6 +61,30 @@ while game:
     
     if finish != True:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= - 1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game = True
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game = True
     
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
     display.update()
     clock.tick(FPS)
